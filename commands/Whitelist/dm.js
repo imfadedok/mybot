3b436dee98de75395e87e53f77e105af595c4527
imfadedok <51056058+imfadedok@users.noreply.github.com>
2019-05-26 21:36:48 +0100

    Add files via upload

---
+++ commands/Whitelist/dm.js
@@ -0,0 +1,28 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: 'dm',
+      enabled: true,
+      runIn: ['text'],
+      cooldown: 2,
+      bucket: 1,
+      aliases: [],
+      permLevel: 10,
+      botPerms: [],
+      requiredConfigs: [],
+      description: 'DMs user(s).',
+      quotedStringSupport: true,
+      usage: '[member:member] <message:str>',
+      usageDelim: '',
+      extendedHelp: null,
+    });
+  }
+
+  async run(msg, [mem = msg.author, ...str]) { 
+    return msg.mentions.users.map(mem => mem.send(str.join(' '))); 
+    msg.author.delete(); 
+ 
+  }
+};
